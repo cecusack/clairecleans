678ba2b29118828d22707754f4f375ba7c25a032
cecusack <cecusack4@gmail.com>
2022-03-07 13:09:36 -0500

    add item sel function

---
+++ R/item_selfunction.R
@@ -0,0 +1,32 @@
+#' Select variables based on means and sds
+#' @param df dataframe.
+#' @param num Number of top means/sds to pull.
+#' @return A list of the variables with the highest means and highest standard deviations.
+#' @export
+#'
+item_sel <- function(df, num) {
+  means <- colMeans(df[,7:ncol(df)], na.rm = TRUE)
+
+  # create column for standard deviations
+  sds <- apply(df[,7:ncol(df)], 2, sd, na.rm = TRUE)
+
+  # organize in dataframe
+  data_itemselection <- data.frame(means, sds)
+
+  # sort by means first in descending order
+  data_itemselection <- data_itemselection[order(-data_itemselection$means),]
+
+  # select top 6 row names
+  topmeans <- data_itemselection %>% rownames_to_column() %>% top_n(num, means) %>% pull(rowname)
+  topmeans # call to fill out DMS individual network log
+  .GlobalEnv$names.means <- topmeans
+  # sort by standard deviation
+  data_itemselection <- data_itemselection[order(-data_itemselection$sds),]
+
+  # select top 6 sd row names
+  topsds <- data_itemselection %>% rownames_to_column() %>% top_n(num, sds) %>% pull(rowname)
+  topsds # call to fill out DMS individual network log
+  .GlobalEnv$names.sd <- topsds
+
+  return(list(topmeans = topmeans, topsds = topsds))
+}
