c3f7059547e7e81897a7796c04cb674b7dd934d4
cecusack <75391842+cecusack@users.noreply.github.com>
2024-12-12 19:29:42 -0500

    Add files via upload
    attempting to get functional

--- R/add_daybeep.R
+++ R/add_daybeep.R
@@ -3,66 +3,60 @@
 #' @import sjmisc
 #' @import purrr
 #' @param df dataframe with time series data.
-#' @param id The name of the column that contains participant ID variable
-#' @param datetime Column name with date and time. Usually generated from Ethica or REDCap
+#' @param idvar The name of the column that contains participant ID variable
+#' @param datetimevar Column name with date and time. Usually generated from Ethica or REDCap
 #' @return Your dataframe with columns numbering the dates (dayvar), times (beepvar), and consecutive beeps (beepconsec).
 #' @export
 #'
-add_daybeep <- function(df, id, datetime){
+add_daybeep <- function(df, idvar, datetimevar){
   object_name <- deparse(substitute(df))
+  datetime <- deparse(substitute(datetime))
 
   # input check
   if(missing(df)) stop("Enter the dataframe that needs a numbered day column and numbered beep column")
-  if(missing(id)) stop("Enter the name of the column that contains participant ID variable in quotes, e.g., 'id'")
-  if(missing(datetime)) stop("Enter the name of the column that contains the date and time field in quotes, e.g., 'Scheduled.Time'")
+  if(missing(idvar)) stop("Enter the name of the column that contains participant ID variable in single quotes, e.g., 'id'")
+  if(missing(datetimevar)) stop("Enter the name of the column that contains the date and time field in single quotes, e.g., 'Scheduled.Time'")
 
-  df[[id]]=factor(df[[id]])
-  df$date <- df[[datetime]]
-  df <- df %>% separate(date, c("date","time"), sep = " ", fill = "right")
-  df$date <- as.Date(df$date, "%Y-%m-%d")
+  df$startdate <- df[[datetimevar]]
+  # df[["startdate"]] <- df[[datetimevar]]  # copy datetime var into new col startdate
+  # df <- df %>% dplyr::relocate(datetimevar, .before= startdate) # move startdate after datetime
 
-  df <- df %>% group_by(id) %>% arrange(lubridate::ymd(date), time) %>% ungroup()
+  # remove timestamp specifier if it exists
+  df$startdate <- ifelse(grepl("\\D$", df$startdate),
+                         gsub("\\s*\\w*$", "", df$startdate),
+                         df$startdate)
 
-  if(length(unique(df[[id]]))>1){
-    list <- lapply(split(df, df[[id]]), data.frame, stringsAsFactors = FALSE)
-    # list <- lapply(list, function(df){df[order(df[[datetime]]),]}) # put rows in order by time
+  df <- df[order(df[[id]], df[["startdate"]]), ] # order by id and datetime
 
-    # add a day var across list
-    for(i in seq_along(list)){
-      list[[i]]$dayvar <- floor(as.numeric(difftime(list[[i]]$date, list[[i]]$date[1], units="days")))+1}
+  df[["starttime"]] <- format(as.POSIXct(df[["startdate"]],format="%Y-%m-%d %H:%M:%S"),"%H:%M:%S") # split into time only
+  df[["startdate"]] <- format(as.POSIXct(df[["startdate"]],format="%Y-%m-%d %H:%M:%S"),"%Y-%m-%d") # split into date only
 
-    # add a beep var across list
-    list <- map(list, ~ .x %>%
-                  group_by(date) %>%
-                  mutate(beepvar = seq(1:n())) %>%
-                  ungroup())
+  list <- split(df, df[[id]])
+  for(i in seq_along(list)){
+    #### day ####
+    list[[i]]$startdate <- lubridate::ymd(list[[i]]$startdate)
+    list[[i]]$day <- (list[[i]]$startdate-first(list[[i]]$startdate))+1
+    list[[i]] <- list[[i]] %>% relocate(day, .after = starttime)
+    list[[i]]$day <- gsub(" days", "", list[[i]]$day)
+    list[[i]]$day <- as.numeric(list[[i]]$day)
 
-    # rbind list back into one dataframe
-    df <- do.call("rbind", list)
-    # move columns to a place I think makes more sense
+    #### beep ####
+    list[[i]] <- list[[i]] %>%
+      group_by(day) %>%
+      mutate(beep=1:n()) %>%
+      ungroup() %>%
+      relocate(beep, .after=day)
 
-    df$num<-1:nrow(df)
-    df$beepconsec <-ave(df$num,df[[id]],FUN = seq_along)
-    df <- df[-which(colnames(df)=="num")]
-
-    datetime=deparse(substitute(datetime))
-    df <- df %>% sjmisc::move_columns(date, time, dayvar, beepvar, .after=datetime)
-    assign(object_name, df, envir = globalenv())
-
-  } else{
-
-    df <- df[order(df[[datetime]]),]
-    # add beep var
-    df <- df %>% group_by(date) %>%
-      mutate(beepvar = seq(1:n())) %>%
-      ungroup()
-    # add consecutive beeps
-    df <- df %>% dplyr::mutate(beepconsec = seq(1:n())) %>% relocate(beepconsec, .after = beepvar)
-    # add day var
-    df$dayvar <- floor(as.numeric(difftime(df$date, df$date[1], units="days")))+1
-
-    df <- df %>% sjmisc::move_columns(date, time, dayvar, beepvar, .after=datetime)
-    assign(object_name, df, envir = globalenv())
+    #### beep consecutive ####
+    list[[i]]$beep_consec <- 1:nrow(list[[i]])
   }
-  return(df %>% select(id, dayvar, beepvar, beepconsec))
+
+  df <- do.call("rbind", list) # list to df
+  row.names(df) <- NULL
+
+  # datetime=deparse(substitute(datetime))
+  df <- df %>% sjmisc::move_columns(startdate, starttime, day, beep, beep_consec, .after={{datetime}})
+
+  return(df)
+  # assign(object_name, df, envir = globalenv())
 }
